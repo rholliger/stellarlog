84400df0ba195d92b3220ac6b1fd0ad410cbeab8
Roybot <roybot@stellarlog.app>
2026-04-06 21:07:13 +0000

    chore: add frontend to pm2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,5 +13,17 @@ module.exports = {
         PYTHONUNBUFFERED: '1',
       },
     },
+    {
+      name: 'stellarlog-frontend',
+      cwd: './frontend',
+      script: 'npm',
+      args: 'run dev',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
   ],
 }
