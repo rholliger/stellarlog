e8b09e9044823f86b35495f226c87260f592d854
Roybot <roybot@stellarlog.app>
2026-04-06 20:37:32 +0000

    chore: remove sky-check-options demo page
    Decision made: using compact card integration on Tonight page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import NewObservation from './pages/NewObservation'
 import TonightSky from './pages/TonightSky'
 import SkyCheck from './pages/SkyCheck'
 import SkyCheckList from './pages/SkyCheckList'
-import SkyCheckOptionsDemo from './pages/SkyCheckOptionsDemo'
 
 function NavBar() {
   const location = useLocation()
@@ -63,7 +62,6 @@ export default function App() {
               <Route path="/" element={<TonightSky />} />
               <Route path="/sky-check" element={<SkyCheck />} />
               <Route path="/sky-checks" element={<SkyCheckList />} />
-              <Route path="/sky-check-options" element={<SkyCheckOptionsDemo />} />
               <Route path="/journal" element={<ObservationsList />} />
               <Route path="/new" element={<NewObservation />} />
               <Route path="/observations/:id" element={<ObservationDetail />} />
